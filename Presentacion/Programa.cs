a836f06a34972b1676f77cf2112b57f7b88849ee
rubenrosas16 <ra.rosas16@info.uas.edu.mx>
2019-10-23 19:28:39 -0600

    Agregar archivos de proyecto.

---
+++ Presentacion/Programa.cs
@@ -0,0 +1,21 @@
+﻿using Presentacion.Formularios;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto
+{
+    class Programa
+    {
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            //Corremos el formulario de inicio
+            Application.Run(new FrmMenu());
+        }
+    }
+}
