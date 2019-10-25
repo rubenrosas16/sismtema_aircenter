df4a2d3d02b744adf591775d5d4941466e5b414d
rubenrosas16 <ra.rosas16@info.uas.edu.mx>
2019-10-25 00:20:29 -0600

    Actualización de estructura 2

--- Presentacion/Formularios/FrmMenu.Designer.cs
+++ Presentacion/Formularios/FrmMenu.Designer.cs
@@ -28,12 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmMenu));
+            this.Textbox1 = new Bunifu.Framework.UI.BunifuTextbox();
+            this.SuspendLayout();
+            // 
+            // Textbox1
+            // 
+            this.Textbox1.BackColor = System.Drawing.Color.White;
+            this.Textbox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Textbox1.BackgroundImage")));
+            this.Textbox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Textbox1.ForeColor = System.Drawing.Color.Silver;
+            this.Textbox1.Icon = ((System.Drawing.Image)(resources.GetObject("Textbox1.Icon")));
+            this.Textbox1.Location = new System.Drawing.Point(12, 12);
+            this.Textbox1.Name = "Textbox1";
+            this.Textbox1.Size = new System.Drawing.Size(218, 42);
+            this.Textbox1.TabIndex = 0;
+            this.Textbox1.text = "Buscar";
+            // 
+            // FrmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(450, 139);
+            this.Controls.Add(this.Textbox1);
+            this.Name = "FrmMenu";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private Bunifu.Framework.UI.BunifuTextbox Textbox1;
     }
 }
